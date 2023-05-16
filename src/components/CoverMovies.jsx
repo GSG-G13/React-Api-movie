7144a7b6f2172c45e810d52c230d5d728a1c44db
= <shathaamin63@gmail.com>
2023-05-17 00:48:10 +0300

    cover movies closes #11

--- src/components/CoverMovies.jsx
+++ src/components/CoverMovies.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React from 'react';
+import Firefly from '../images/Firefly.jpg'
 
-class CoverMovies extends React.Component {
+class  CoverMovies  extends React.Component {
+render() {
+
+  return (
+    <section id="intro">
+      <div id="img-container">
+        <img className='cover-img' src={Firefly} alt="bgggg" />
+      </div>
+    </section>
+  );
+
+};
 }
 
 export default CoverMovies;
