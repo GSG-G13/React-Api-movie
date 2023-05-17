25a24b81ecba2a18013caef636da3a6ff3443444
= <shathaamin63@gmail.com>
2023-05-17 11:40:03 +0300

    css Relates #18

--- src/components/FilterMovies.jsx
+++ src/components/FilterMovies.jsx
@@ -1,54 +1,36 @@
 import React from "react";
 
 class FilterMovies extends React.Component {
+  state = {
+    filterMovies: "All",
+  };
 
+  handleFilter = (value) => {
+    this.setState({ filterMovies: value });
+    this.props.genresSelected(value); 
+  };
 
-state = {
-    filterMovies : ''
+  render() {
+    return (
+      <section id="filter-movies">
+        <h2>Select Your Favorite Movie</h2>
+        <p>Movies filtered by genres.</p>
+        <select
+          type="text"
+          name="filterMovies"
+          onChange={(e) => this.handleFilter(e.target.value)}
+          value={this.state.filterMovies}
+        >
+          <option value="All">All</option>
+          <option value="Drama">Drama</option>
+          <option value="Action">Action</option>
+          <option value="Science-Fiction">Science-Fiction</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Crime">Crime</option>
+        </select>
+      </section>
+    );
+  }
 }
-    handleFilter = (value) => {
-        this.setState({filterMovies: value})
-    }
-
-    componentDidUpdate = (prevProps, prevState) => {
-        if (this.state.filterMovies !== prevState.filterMovies) {
-            this.props.genresSelected(this.state.filterMovies)
-        }
-    }
-
-
-
-
-
-
-render() {
-    return ( 
-        <section id="filter-movies">
-            <h2>Select Your Favorite Movie</h2>
-            <p>Movies filtered by genres.</p>
-            <select
-            type="text"
-            name="filterMovies"
-            onChange={(e) => this.handleFilter(e.target.value)}
-            value={this.state.searchQuery}
-            >
-                <option>All</option>
-                <option>Drama</option>
-                <option>Action</option>
-                <option>Science-Fiction</option>
-                <option>Thriller</option>
-                <option>Crime</option>
-            </select>
-            
-            
-        </section>
-        );
-
-}
-
-
-
-}
-
 
 export default FilterMovies;
