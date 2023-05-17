6b0f80a7814c0db4f340ae7872c39db7035d4307
Heba-Adwan <hebawael1575@gmail.com>
2023-05-17 12:12:11 +0300

    Fix Conflict

--- src/components/FilterMovies.jsx
+++ src/components/FilterMovies.jsx
@@ -33,46 +33,7 @@ class FilterMovies extends React.Component {
   }
 }
 
-    handleFilter = (value) => {
-        this.setState({filterMovies: value})
-        this.props.genresSelected(value)
-
-    }
-
-
-
-
-
-
-
-render() {
-    return ( 
-        <section id="filter-movies">
-            <h2>Select Your Favorite Movie</h2>
-            <p>Movies filtered by genres.</p>
-            <select
-            type="text"
-            name="filterMovies"
-            onChange={(e) => this.handleFilter(e.target.value)}
-            value={this.state.filterMovies}
-            >
-                <option>All</option>
-                <option>Drama</option>
-                <option>Action</option>
-                <option>Science-Fiction</option>
-                <option>Thriller</option>
-                <option>Crime</option>
-            </select>
-            
-            
-        </section>
-        );
-
-}
-
-
-
-}
+   
 
 
 export default FilterMovies;
